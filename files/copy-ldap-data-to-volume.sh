db15991857016acdf773d52591873a0d446a6609
gabriel.silva <gabriel.silva@fiocruz.br>
2018-12-25 10:48:28 -0200

    .

--- files/copy-ldap-data-to-volume.sh
+++ files/copy-ldap-data-to-volume.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -d "/data/schema"]; then
+if [[ ! -d "/data/schema" ]]; then
   cp -Ra /etc/openldap/* /data/
   cp -Ra /var/lib/openldap/openldap-data/* /ldap-db-data/
   chown -R ldap:ldap /data /ldap-db-data
